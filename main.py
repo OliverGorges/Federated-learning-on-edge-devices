1fa29f060eeb3bc609a11a2b62eae7f84aae5481
Oliver Gorges <oliver.a.gorges@capgemini.com>
2020-07-08 07:21:11 +0100

    improve lag in VideoStream

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from PIL import Image, ImageOps
 from concurrent.futures import ThreadPoolExecutor
 from utils.ThermalImage.camera import Camera as ThermalCamera
 from utils.Tensorflow.detection import FaceDetection
-from utils.OpenCV.postprocess import drawBoxes
+from utils.Tensorflow.postprocess import drawBoxes
 from utils.OpenCV.camera import Camera
 from utils.ThermalImage.postprocess import dataToImage
 from utils.Dataset.annotation import jsonAnnotaions
 from utils.fps import FPS
 from io import BytesIO
 #Log
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 #Dataset config
 dataset = os.path.join(os.getcwd(), "Dataset")
@@ -43,8 +43,8 @@ termalfaces = []
 
 # Init OpenCV
 c = Camera()
-faceDetection = FaceDetection()
-
+faceDetection1 = FaceDetection(model="TFlite", modelType="tflite")
+faceDetection2 = FaceDetection(model="TFlite", modelType="tflite")
 # Image queue to delay thermal Image
 data_queue = []
 delay = 0
@@ -53,50 +53,60 @@ tick = 0
 c1, c2 = 0, 0
 
 def thermalImageThread():
+    t1 = time.time()
     #get Data
     image_data = tc.getTemperatureImage()
-    logging.debug(type(image_data))
+    #logging.debug(type(image_data))
 
     #get Image
     thermalImg = dataToImage(image_data, outputSize)
-
+    t2 = time.time()
     #predict faces
-    faceDetection.prepareImage(thermalImg, 1)
-    faces = []#faceDetection.detectFace()
+    faceDetection1.prepareImage(thermalImg, 1)
+    faces = faceDetection1.detectFace()
     # Add data to Q
     data_queue.append((thermalImg, faces, image_data))
+    logging.debug(f"Runtime ThermalThread: {time.time() - t1} / Image {t2 - t1} / Prediction {time.time() - t2}")
 
-def imageThread():
-    # Capture frame-by-frame
-    frame = c.takeImage()
+def imageThread(frame):
+    t1 = time.time()
     # Crop image from PiCam2
-    frame, size = c.cropFrame(frame)
-     #Detect Faces
+    frame, size = c.cropImage(frame)
+    #Detect Faces
+    t2 = time.time()
     m = 1  #Multiplicator to reduce model imput size
-    faceDetection.prepareImage(frame, m)
-    faces = faceDetection.detectFace()
+    faceDetection2.prepareImage(frame, m)
+    faces = faceDetection2.detectFace()
+    
+    logging.debug(f"Runtime ImageThread:  {time.time() - t1} / Image {t2 - t1} / Prediction {time.time() - t2}")
     return frame, size, faces
+
+    
 
 # sample image
 frame = c.takeImage()
 oSize = c.shape #Get the size of the original Image
 
 thermalThread = None
-
+ImageThread = None
+frame, size, faces = None, (0,0), []
 with ThreadPoolExecutor(max_workers=4) as executor:
     while True:
-        print(".")
+        # Capture frame-by-frame
+        liveFrame = c.takeImage()
+
         #print(f'Tasks: {executor.getActiveCount()}')
         if thermalThread is None:
-            print("start")
             thermalThread = executor.submit(thermalImageThread)
         if thermalThread.done():
-            print("done")
             thermalThread = None
             #thermalThread = executor.submit(getThermalImage) #Take Thermal Image and add it to the Q
-        print("start image")
-        ImageThread = executor.submit(imageThread)
-        frame, size, faces = ImageThread.result()
+
+        if ImageThread is None:
+            ImageThread = executor.submit(imageThread, liveFrame)
+        if ImageThread.done():
+            frame, size, faces = ImageThread.result()
+            ImageThread = None
     
         #thermalFaces = faceDetection.detectFace(thermal=True)
         data = None
@@ -111,23 +121,24 @@ with ThreadPoolExecutor(max_workers=4) as executor:
 
         c1 += 1
 
-        # Prepare OpenCV Image
-        frame = cv2.resize(frame, outputSize , interpolation = cv2.INTER_AREA)
+        if frame is not None:
+            # Prepare OpenCV Image
+            frame = cv2.resize(frame, outputSize , interpolation = cv2.INTER_AREA)
 
-        prevFrame = frame.copy()
-        # Draw a rectangle around the faces
-        prevFrame = drawBoxes(prevFrame, faces)
+            prevFrame = frame.copy()
+            # Draw a rectangle around the faces
+            prevFrame = drawBoxes(prevFrame, faces)
 
-        if thermalImg is not None:
-            prevFrame = drawBoxes(prevFrame, thermalFaces, (0,0,255))
-            alpha = 0.6
-            prevFrame = cv2.addWeighted(prevFrame, alpha, thermalImg, 1-alpha, 0.0)
+            if thermalImg is not None:
+                prevFrame = drawBoxes(prevFrame, thermalFaces, (0,0,255))
+                alpha = 0.6
+                prevFrame = cv2.addWeighted(prevFrame, alpha, thermalImg, 1-alpha, 0.0)
 
-        cv2.putText(prevFrame, f'FPS: {int(fps)}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
-        print(int(fps))
+            cv2.putText(prevFrame, f'FPS: {int(fps)}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+            print(int(fps))
 
-        # Display the resulting frame
-        cv2.imshow('Video', prevFrame)
+            # Display the resulting frame
+            cv2.imshow('Video', prevFrame)
 
         resetGap = resetGap - 1
         if resetGap <= 0:
@@ -139,7 +150,7 @@ with ThreadPoolExecutor(max_workers=4) as executor:
         print (f'{c1} / {c2}')
 
         #out.write(frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 
--- utils/Tensorflow/detection.py
+++ utils/Tensorflow/detection.py
@@ -18,15 +18,18 @@ from object_detection.utils import visualization_utils as vis_util
 
 class FaceDetection():
 
+
     """
     model: model that should be Preloaded
     isTFLite: Selection if the used model in TF or TFLite
     """
-    def __init__(self, model="ssd_mobilenet_v2_coco_2018_03_29", isTFLite=False):
-        self.isTFLite = isTFLite
-        modelDir = os.path.join('Traindata', 'model', model,"saved_model" )
+    def __init__(self, model="ssd_mobilenet_v2_coco_2018_03_29", modelType="savedmodel"):
+        modelDir = os.path.join('Traindata', 'model', model )
         print(f'Load Model from: {modelDir}')
-        if isTFLite:
+        self.modelType = modelType
+        tf.compat.v1.enable_eager_execution
+
+        if self.modelType == "tflite":
             model = ''
             for f in os.listdir(modelDir):
                 if f.endswith('.tflite'):
@@ -34,9 +37,31 @@ class FaceDetection():
                     break
             if model == '': raise Exception(f'No TFLite model found in {modelDir}')
             
-        else:
-            self.model = tf.compat.v2.saved_model.load(modelDir, None)
+            # load model
+            self.interpreter = tf.lite.Interpreter(model_path=model)
+            self.interpreter.allocate_tensors()
+            self.input_details = self.interpreter.get_input_details()
+            self.output_details = self.interpreter.get_output_details()
+
+        elif self.modelType == "savedmodel":
+            model = os.path.join(modelDir, "saved_model" )
+            self.model = tf.compat.v2.saved_model.load(model, None)
             self.model = self.model.signatures['serving_default']
+        elif self.modelType == "graph":
+            model = ''
+            for f in os.listdir(modelDir):
+                if f.endswith('.pb'):
+                    model = os.path.join(modelDir, f)
+                    break
+            print(model)
+            detection_graph = tf.Graph()
+            with detection_graph.as_default():
+                od_graph_def = tf.GraphDef()
+                with tf.gfile.GFile(model, 'rb') as fid:
+                    serialized_graph = fid.read()
+                    od_graph_def.ParseFromString(serialized_graph)
+                    tf.import_graph_def(od_graph_def, name='')
+            self.model = detection_graph
             
 
     def prepareImage(self, image, scale):
@@ -50,11 +75,15 @@ class FaceDetection():
     def detectFace(self, image=None, normalized=True):
         if not image:
             image = self.image
-        logging.debug(image)
-        if self.isTFLite:
+        #logging.debug(image)
+        if self.modelType == "tflite":
+            self.detections = self.run_inference_on_tflite(image)
             pass
-        else:
-            self.faces = self.run_inference_for_single_image(self.model, image)
+        elif self.modelType == "savedmodel":
+            self.detections = self.run_inference_on_saved_model(self.model, image)
+        elif self.modelType == "graph":
+            self.detections = self.run_inference_on_graph(self.model, image)
+        return self.detections
 
 
     def normalizeBoxes(self, image=None, faces=None):
@@ -63,7 +92,7 @@ class FaceDetection():
         else:
             width, height = image.shape[:2]
         if not faces:
-            faces = self.faces
+            faces = self.detections
 
         normFaces = []
         for (x, y, w, h) in faces:
@@ -77,7 +106,56 @@ class FaceDetection():
             )
         return normFaces
 
-    def run_inference_for_single_image(self, model, image):
+    def run_inference_on_graph(self, graph, image):
+        with graph.as_default():
+            with tf.Session() as sess:
+                print("Prepare Model")
+                # Get handles to input and output tensors
+                ops = tf.get_default_graph().get_operations()
+                all_tensor_names = {output.name for op in ops for output in op.outputs}
+                tensor_dict = {}
+                for key in [
+                    'num_detections', 'detection_boxes', 'detection_scores',
+                    'detection_classes', 'detection_masks'
+                ]:
+                    tensor_name = key + ':0'
+                    if tensor_name in all_tensor_names:
+                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                            tensor_name)
+                if 'detection_masks' in tensor_dict:
+                    # The following processing is only for single image
+                    detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+                    detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                    # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                    real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                    detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                    detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                        detection_masks, detection_boxes, image.shape[0], image.shape[1])
+                    detection_masks_reframed = tf.cast(
+                        tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                    # Follow the convention by adding back the batch dimension
+                    tensor_dict['detection_masks'] = tf.expand_dims(
+                        detection_masks_reframed, 0)
+                image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+                print("Run inference")
+                output_dict = sess.run(tensor_dict,
+                                        feed_dict={image_tensor: np.expand_dims(image, 0)})
+                
+                # all outputs are float32 numpy arrays, so convert types as appropriate
+                output_dict['num_detections'] = int(output_dict['num_detections'][0])
+                output_dict['detection_classes'] = output_dict[
+                    'detection_classes'][0].astype(np.uint8)
+                output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+                output_dict['detection_scores'] = output_dict['detection_scores'][0]
+                if 'detection_masks' in output_dict:
+                    output_dict['detection_masks'] = output_dict['detection_masks'][0]
+               
+        return output_dict
+
+
+    def run_inference_on_saved_model(self, model, image):
     
         image = np.asarray(image)
         # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
@@ -90,7 +168,8 @@ class FaceDetection():
         # All outputs are batches tensors.
         # Convert to numpy arrays, and take index [0] to remove the batch dimension.
         # We're only interested in the first num_detections.
-        num_detections = int(output_dict.pop('num_detections'))
+        num_detections = output_dict.pop('num_detections')
+        print(num_detections.numpy())
         output_dict = {key:value[0, :num_detections].numpy() 
                         for key,value in output_dict.items()}
         output_dict['num_detections'] = num_detections
@@ -109,3 +188,29 @@ class FaceDetection():
             output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
         
         return output_dict
+
+    def run_inference_on_tflite(self, img):
+
+        img_org = img.copy()
+
+        #prepare Image
+        img = cv2.resize(img, (300, 300))
+        img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) # (1, 300, 300, 3)
+        img = img.astype(np.float32)
+
+
+        # set input tensor
+        self.interpreter.set_tensor(self.input_details[0]['index'], img)
+
+
+        # run
+        self.interpreter.invoke()
+        output_dict = {}
+        # get outpu tensor
+        output_dict['detection_boxes'] = self.interpreter.get_tensor(self.output_details[0]['index'])
+        output_dict['detection_classes'] = self.interpreter.get_tensor(self.output_details[1]['index'])
+        output_dict['detection_scores'] = self.interpreter.get_tensor(self.output_details[2]['index'])
+        output_dict['num_detections'] = int(self.interpreter.get_tensor(self.output_details[3]['index']))
+
+        return output_dict
+
