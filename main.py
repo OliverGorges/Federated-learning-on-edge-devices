b5505a86b6ec46a1cd189f5988768b7e83712f7e
Oliver Gorges <oliver.a.gorges@capgemini.com>
2020-07-08 11:01:05 +0100

    slight performance improvements

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ from utils.ThermalImage.postprocess import dataToImage
 from utils.Dataset.annotation import jsonAnnotaions
 from utils.fps import FPS
 from io import BytesIO
+from multiprocessing import Process
+
 #Log
 logging.basicConfig(level=logging.DEBUG)
 
@@ -57,82 +59,75 @@ def thermalImageThread():
     #get Data
     image_data = tc.getTemperatureImage()
     #logging.debug(type(image_data))
-
     #get Image
     thermalImg = dataToImage(image_data, outputSize)
-    t2 = time.time()
-    #predict faces
-    faceDetection1.prepareImage(thermalImg, 1)
-    faces = faceDetection1.detectFace()
-    # Add data to Q
-    data_queue.append((thermalImg, faces, image_data))
-    logging.debug(f"Runtime ThermalThread: {time.time() - t1} / Image {t2 - t1} / Prediction {time.time() - t2}")
+    logging.debug(f"Runtime ThermalThread: {time.time() - t1}")
+    return thermalImg, image_data
 
 def imageThread(frame):
     t1 = time.time()
-    # Crop image from PiCam2
-    frame, size = c.cropImage(frame)
     #Detect Faces
     t2 = time.time()
-    m = 1  #Multiplicator to reduce model imput size
-    faceDetection2.prepareImage(frame, m)
-    faces = faceDetection2.detectFace()
     
     logging.debug(f"Runtime ImageThread:  {time.time() - t1} / Image {t2 - t1} / Prediction {time.time() - t2}")
     return frame, size, faces
 
-    
+def detectionThread(inputDict, detector):
+    t1 = time.time()
+    detector.prepareImage(inputDict["Image"], inputDict["Scale"])
+    inputDict["Faces"] = detector.detectFace()
+    logging.debug(f"Runtime DetectionThread:  {time.time() - t1} ")
+    return inputDict
 
 # sample image
 frame = c.takeImage()
 oSize = c.shape #Get the size of the original Image
 
 thermalThread = None
-ImageThread = None
-frame, size, faces = None, (0,0), []
+detectionThread1,  detectionThread2 = None, None 
+
+frameDict = {}
+
 with ThreadPoolExecutor(max_workers=4) as executor:
     while True:
         # Capture frame-by-frame
         liveFrame = c.takeImage()
+        # Crop image from PiCam2
+        liveFrame, size = c.cropImage(liveFrame)
 
         #print(f'Tasks: {executor.getActiveCount()}')
         if thermalThread is None:
             thermalThread = executor.submit(thermalImageThread)
         if thermalThread.done():
-            thermalThread = None
-            #thermalThread = executor.submit(getThermalImage) #Take Thermal Image and add it to the Q
+            if detectionThread1 is None and detectionThread2 is None:
+                c2 += 1
+                thermalImg, data = thermalThread.result()
+                detectionThread1 = executor.submit(detectionThread, {"Image": liveFrame, "Size": size, "Scale": 1}, faceDetection1)
+                detectionThread2 = executor.submit(detectionThread, {"Image": thermalImg, "Data": data, "Scale": 1}, faceDetection2)
+                thermalThread = None
 
-        if ImageThread is None:
-            ImageThread = executor.submit(imageThread, liveFrame)
-        if ImageThread.done():
-            frame, size, faces = ImageThread.result()
-            ImageThread = None
-    
-        #thermalFaces = faceDetection.detectFace(thermal=True)
-        data = None
 
-        # Transform and Delay Thermal Image to synchronice with OpenCV Image (Delay ~1s)
-        if len(data_queue) > delay:
-            c2 += 1
-            tick = 0
-            x = data_queue.pop(0)
-            thermalImg, thermalFaces, data = x
-            #cv2.imshow('ThermalVideo', thermalImg)
-
+        if detectionThread1 is not None and detectionThread2 is not None:
+            if detectionThread1.done() and detectionThread2.done():
+                frameDict = {}
+                frameDict["PICam"] = detectionThread1.result()
+                frameDict["ThermalCam"] = detectionThread2.result()
+                detectionThread1, detectionThread2 = None, None
+          
         c1 += 1
 
-        if frame is not None:
+        if "PICam" in frameDict:
             # Prepare OpenCV Image
-            frame = cv2.resize(frame, outputSize , interpolation = cv2.INTER_AREA)
+            frame = cv2.resize(frameDict["PICam"]["Image"], outputSize , interpolation = cv2.INTER_AREA)
 
             prevFrame = frame.copy()
             # Draw a rectangle around the faces
-            prevFrame = drawBoxes(prevFrame, faces)
+            prevFrame = drawBoxes(prevFrame, frameDict["PICam"]["Faces"])
 
-            if thermalImg is not None:
-                prevFrame = drawBoxes(prevFrame, thermalFaces, (0,0,255))
-                alpha = 0.6
-                prevFrame = cv2.addWeighted(prevFrame, alpha, thermalImg, 1-alpha, 0.0)
+            
+            prevFrame = drawBoxes(prevFrame, frameDict["ThermalCam"]["Faces"], (0,0,255))
+            alpha = 0.6
+            prevFrame = cv2.addWeighted(prevFrame, alpha, frameDict["ThermalCam"]["Image"], 1-alpha, 0.0)
 
             cv2.putText(prevFrame, f'FPS: {int(fps)}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
             print(int(fps))
@@ -150,7 +145,7 @@ with ThreadPoolExecutor(max_workers=4) as executor:
         print (f'{c1} / {c2}')
 
         #out.write(frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
 
