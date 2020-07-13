b457f997ea0b895500da8873db3ea18d09c41e3d
Oliver Gorges <oliver.a.gorges@capgemini.com>
2020-07-13 16:01:10 +0200

    add xmlcoco and tflite converter

--- utils/Dataset/cocoAnnotationConverter.py
+++ utils/Dataset/cocoAnnotationConverter.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.ElementTree as ET
 import os
 import logging
 from datetime import datetime
@@ -7,76 +8,165 @@ import pathlib
 # Coco Dataformat
 #https://cocodataset.org/#format-data
 
+class JsonConverter():
 
-def convert(imageDir, annotationDir, labelmap, outputPath, split=0.7):
-  annotationFiles = os.listdir(os.path.join(annotationDir))
-  if not split == 1:
-    sub = [annotationFiles[0:int(len(annotationFiles)*split)], annotationFiles[int(len(annotationFiles)*split):]]
-    train = createAnnotation(imageDir, sub[0], annotationDir, labelmap, outputPath, "Train")
-    eval = createAnnotation(imageDir, sub[1], annotationDir, labelmap, outputPath, "Eval")
-    return [train, eval], [len(sub[0]), len(sub[1])]
-  else:
-    train = createAnnotation(imageDir, annotationFiles, annotationDir, labelmap, outputPath, "Train")
-    eval = ""
-    return [train, eval], [len(annotationFiles), 0]
+  def convert(self, imageDir, annotationDir, outputPath, labelmap=None, split=0.7):
+    annotationFiles = os.listdir(os.path.join(annotationDir))
+    if not split == 1:
+      sub = [annotationFiles[0:int(len(annotationFiles)*split)], annotationFiles[int(len(annotationFiles)*split):]]
+      train = self.createAnnotation(imageDir, sub[0], annotationDir, labelmap, outputPath, "Train")
+      eval = self.createAnnotation(imageDir, sub[1], annotationDir, labelmap, outputPath, "Eval")
+      return [train, eval], [len(sub[0]), len(sub[1])]
+    else:
+      train = self.createAnnotation(imageDir, annotationFiles, annotationDir, labelmap, outputPath, "Train")
+      eval = ""
+      return [train, eval], [len(annotationFiles), 0]
 
-def createAnnotation(imageDir, annotationFiles, annotationDir, labelmap, outputPath, tag="Train"):
-  coco = {}
-  images = []
-  annotations = []
-  categories = []
+  def createAnnotation(self, imageDir, annotationFiles, annotationDir, labelmap, outputPath, tag="Train"):
+    coco = {}
+    images = []
+    annotations = []
+    categories = []
 
-  nrOfAnnotations = 0
+    nrOfAnnotations = 0
 
-  categories.append({
-      "supercategory": "none",
-      "name": "face",
-      "id": 0
-    })
+    categories.append({
+        "supercategory": "none",
+        "name": "face",
+        "id": 0
+      })
 
-  for i, fname in enumerate(annotationFiles):
-    with open(os.path.join(annotationDir, fname)) as json_file:
-      anno = json.load(json_file)
-      imageHeight = 60*8
-      imageWidth = 80*8
+    for i, fname in enumerate(annotationFiles):
+      with open(os.path.join(annotationDir, fname)) as json_file:
+        anno = json.load(json_file)
+        imageHeight = 60*8
+        imageWidth = 80*8
+        images.append({
+          "file_name": str(pathlib.Path(os.path.join(imageDir, anno["thermalImage"])).absolute()),
+          "height": imageHeight,
+          "width": imageWidth,
+          "id": i
+        })
+        for box in anno["objects"]:
+          obj = {}
+          obj["id"] = nrOfAnnotations
+          box = [int(box["bbox"]["xmin"] * imageWidth), int(box["bbox"]["ymin"] * imageHeight), int((box["bbox"]["xmax"] - box["bbox"]["xmin"]) * imageWidth), int((box["bbox"]["ymax"] - box["bbox"]["ymin"]) * imageHeight)]
+          obj["bbox"] = box
+          obj["area"] = box[2] * box[3]
+          obj["image_id"] = i
+          obj["iscrowd"] = 1 if len(anno["objects"]) > 1 else 0
+          obj["category_id"] = 0
+          annotations.append(obj)
+          nrOfAnnotations += 1
+
+
+    coco["info"] = {
+      "description": "Thermal FaceDetection",
+      "version": "1.0",
+      "year": 2020,
+      "contributor": "Oliver Gorges",
+      "date_created": datetime.now().strftime("%d/%m/%Y")
+    }
+
+    coco["type"] = "instances"
+    coco["images"] = images
+    coco["annotations"] = annotations
+    coco["categories"] = categories
+    output = os.path.join(outputPath, f'coco{tag}{datetime.now().strftime("%d%m%Y")}.json')
+    with open(output, 'w') as outfile:
+        json.dump(coco, outfile)
+
+    return output
+
+
+class XmlConverter():
+
+
+  def convert(self, imageDir, annotationDir, outputPath, labelmap=None, split=0.7):
+    annotationFiles = os.listdir(os.path.join(annotationDir))
+    if not split == 1:
+      sub = [annotationFiles[0:int(len(annotationFiles)*split)], annotationFiles[int(len(annotationFiles)*split):]]
+      train = self.createAnnotation(imageDir, sub[0], annotationDir, labelmap, outputPath, "Train")
+      eval = self.createAnnotation(imageDir, sub[1], annotationDir, labelmap, outputPath, "Eval")
+      return [train, eval], [len(sub[0]), len(sub[1])]
+    else:
+      train = self.createAnnotation(imageDir, annotationFiles, annotationDir, labelmap, outputPath, "Train")
+      eval = ""
+      return [train, eval], [len(annotationFiles), 0]
+
+
+  def createAnnotation(self, imageDir, annotationFiles, annotationDir, labelmap, outputPath, tag="Train"):
+    coco = {}
+    images = []
+    annotations = []
+    categories = []
+    
+    
+    nrOfAnnotations = 0
+    labelDict = {}
+    if labelmap is not None:
+      labels = ET.parse(labelmap).getroot()
+      for label in labels.findall('label'):
+        labelDict[str(label.find('name').text)] = int(label.find('id').text)
+        categories.append({
+        "supercategory": label.find('supercategory').text,
+        "name":  label.find('name').text,
+        "id":  int(label.find('id').text)
+      })
+    else:
+      labels["face"] = 0
+      categories.append({
+          "supercategory": "none",
+          "name": "face",
+          "id": 0
+        })
+
+    for i, fname in enumerate(annotationFiles):
+      
+      anno = ET.parse(os.path.join(annotationDir, fname)).getroot()
       images.append({
-        "file_name": str(pathlib.Path(os.path.join(imageDir, anno["thermalImage"])).absolute()),
-        "height": imageHeight,
-        "width": imageWidth,
+        "file_name": str(pathlib.Path(os.path.join(imageDir, anno.find("filename").text)).absolute()),
+        "height": int(anno.find('size').find('height').text),
+        "width": int(anno.find('size').find('width').text),
         "id": i
       })
-      for box in anno["objects"]:
+      for detection in anno.findall('object'):
+        box = detection.find("bndbox")
         obj = {}
         obj["id"] = nrOfAnnotations
-        box = [int(box["bbox"]["xmin"] * imageWidth), int(box["bbox"]["ymin"] * imageHeight), int((box["bbox"]["xmax"] - box["bbox"]["xmin"]) * imageWidth), int((box["bbox"]["ymax"] - box["bbox"]["ymin"]) * imageHeight)]
+        box = [int(box.find("xmin").text), int(box.find("ymin").text), int(box.find("xmax").text), int(box.find("ymax").text)]
         obj["bbox"] = box
         obj["area"] = box[2] * box[3]
         obj["image_id"] = i
-        obj["iscrowd"] = 1 if len(anno["objects"]) > 1 else 0
-        obj["category_id"] = 0
+        obj["iscrowd"] = 1 if len(anno.findall('object')) > 1 else 0
+        obj["category_id"] = labelDict[detection.find('name').text]
         annotations.append(obj)
         nrOfAnnotations += 1
 
 
-  coco["info"] = {
-    "description": "Thermal FaceDetection",
-    "version": "1.0",
-    "year": 2020,
-    "contributor": "Oliver Gorges",
-    "date_created": datetime.now().strftime("%d/%m/%Y")
-  }
+    coco["info"] = {
+      "description": "Thermal FaceDetection",
+      "version": "1.0",
+      "year": 2020,
+      "contributor": "Oliver Gorges",
+      "date_created": datetime.now().strftime("%d/%m/%Y")
+    }
 
-  coco["type"] = "instances"
-  coco["images"] = images
-  coco["annotations"] = annotations
-  coco["categories"] = categories
-  output = os.path.join(outputPath, f'coco{tag}{datetime.now().strftime("%d%m%Y")}.json')
-  with open(output, 'w') as outfile:
-      json.dump(coco, outfile)
+    coco["type"] = "instances"
+    coco["images"] = images
+    coco["annotations"] = annotations
+    coco["categories"] = categories
+    output = os.path.join(outputPath, f'coco{tag}{datetime.now().strftime("%d%m%Y")}.json')
+    with open(output, 'w') as outfile:
+        json.dump(coco, outfile)
 
-  return output
-  
+    return output
+
+
 
 if __name__ == "__main__":
-  convert(os.path.join("..", "..", "Dataset", "Images"), os.path.join("..", "..", "Dataset", "Annotations"), None, os.path.join("..", "..", "Traindata", "data", "tfrecords")  )
+  #JsonConverter().convert(os.path.join("..", "..", "Dataset", "Images"), os.path.join("..", "..", "Dataset", "Annotations"), None, os.path.join("..", "..", "Traindata", "data", "tfrecords")  )
+  
+  folder = os.path.join(os.sep,"Users","olgorges","Downloads","667889_1176415_bundle_archive")
+  XmlConverter().convert( os.path.join(folder,"images"), os.path.join(folder,"annotations"), os.path.join(folder,"output"), os.path.join(folder,"label_map.xml") )
   print(f'Saved in {pathlib.Path(os.path.join("..", "..", "Dataset")).absolute()}')
---
+++ utils/Tensorflow/tfliteConverter.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+import os
+from google.protobuf import text_format
+from object_detection import export_tflite_ssd_graph_lib
+from object_detection.protos import pipeline_pb2
+
+
+def convertModel(input_dir, output_dir, pipeline_config="", checkpoint:int=0, ):
+
+    files = os.listdir(input_dir)
+    if pipeline_config == "":
+        pipeline_config = [pipe for pipe in files if pipe.endswith(".config")][0]
+    pipeline_config_path = os.path.join(input_dir, pipeline_config)
+
+    ckeckpint_file = [chck for chck in files if chck.endswith(f"{checkpoint}.meta")][0]
+    trained_checkpoint_prefix = os.path.join(input_dir, ckeckpint_file[:-5])
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    export_tflite_ssd_graph(pipeline_config_path, trained_checkpoint_prefix, output_dir)
+
+    input_array = ["normalized_input_image_tensor"]
+    output_array = ['TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3']
+    
+    converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
+        os.path.join(output_dir, "tflite_graph.pb"), 
+        input_array, 
+        output_array,
+        input_shapes={'normalized_input_image_tensor':[1, 300, 300, 3]}
+        )
+
+    converter.allow_custom_ops = True
+    tflite_model = converter.convert()
+    open(os.path.join(output_dir, "converted_model.tflite"), "wb").write(tflite_model)
+
+"""
+    #Prepare Model for TFlite
+    tflite_convert --graph_def_file Federated-learning-on-edge-devices/Traindata/model/TFlite/tflite_graph.pb --output_file=./detect.tflite --output_format=TFLITE --input_shapes=1,300,300,3 --input_arrays=normalized_input_image_tensor --output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3' --allow_custom_ops
+"""
+
+
+def export_tflite_ssd_graph(pipeline_config_path, trained_checkpoint_prefix, output_directory, use_regular_nms=False, max_classes_per_detection=2, max_detections=10, add_postprocessing_op=True ):
+    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+
+    with tf.gfile.GFile(pipeline_config_path, 'r') as f:
+        text_format.Merge(f.read(), pipeline_config)
+    #text_format.Merge(FLAGS.config_override, pipeline_config)
+    export_tflite_ssd_graph_lib.export_tflite_graph(
+        pipeline_config, trained_checkpoint_prefix, output_directory,
+        add_postprocessing_op, max_detections,
+        max_classes_per_detection, use_regular_nms)
+
+if __name__ == "__main__":
+    inputDir = os.path.join("Traindata", "model", "FaceDetect")
+    outputDir = os.path.join("Traindata", "model", "TFLite")
+    convertModel(inputDir, outputDir, checkpoint=5081)
