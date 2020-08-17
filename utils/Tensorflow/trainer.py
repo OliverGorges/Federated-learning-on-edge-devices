44c05d6f86d5eb1501d3bcf9047c5300dc8fdb6b
Oliver Gorges <oliver.a.gorges@capgemini.com>
2020-08-17 14:14:39 +0200

    last update

--- utils/Tensorflow/trainer.py
+++ utils/Tensorflow/trainer.py
@@ -385,6 +385,16 @@ def eval( modelDir, dataDir, tfRecordsConfig=None, model="ssd_mobilenet_v2_coco_
         wait_interval=300, timeout=3600)
 
 
-
+def readEval(evalDir, tags = ['DetectionBoxes_Percision/mAP', 'DetectionBoxes_Recall/AR@1', 'loss', 'Loss/classification_loss', 'Loss/localization_loss', 'Loss/regularization_loss', 'Loss/total_loss']):
+    meta = {}    
+    evalFiles = os.listdir(evalDir)
+    print(evalFiles)
+    evalMeta = {}
+    for i, evalFile in enumerate(evalFiles):
+        for e in tf.compat.v1.train.summary_iterator(os.path.join(evalDir, evalFile)):
+            for v in e.summary.value:
+                evalMeta[v.tag] = v.simple_value
+        break
+    return evalMeta
 
 
